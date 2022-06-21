42e94f405cf6fb47be3202f241c86bba63ff8587
pinanek23 <hoangsonngo555@gmail.com>
2022-06-21 18:23:38 +0700

    Added Header

--- src/components/header/theme-button.tsx
+++ src/components/header/theme-button.tsx
@@ -48,14 +48,7 @@ const ThemeButton: Component = () => {
   })
 
   return (
-    <Show
-      when={colorMode() !== 'none'}
-      fallback={
-        <svg class={classnames(Styles.size, Styles.svg)} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm0-1.5v-17a8.5 8.5 0 0 1 0 17Z" />
-        </svg>
-      }
-    >
+    <Show when={colorMode() !== 'none'} fallback={<span class={classnames(Styles.size, Styles.placeHolder)} />}>
       <button
         class={classnames(Styles.size, Styles.button)}
         aria-label={label()}
