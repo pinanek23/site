8df98c6987cf2cf927883777097cf01ecad53807
pinanek23 <hoangsonngo555@gmail.com>
2022-06-22 18:51:17 +0700

    Update post content styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from 'astro/config'
 import { astroImageTools } from 'astro-imagetools'
 import solid from '@astrojs/solid-js'
+
 import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
 
 export default defineConfig({
   integrations: [solid(), astroImageTools],
+
   experimental: {
     integrations: true
+  },
+
+  markdown: {
+    remarkPlugins: ['remark-gfm', ['remark-smartypants', { dashes: false }]],
+    rehypePlugins: ['rehype-external-links'],
+
+    shikiConfig: {
+      theme: 'dark-plus'
+    }
   },
 
   vite: {
