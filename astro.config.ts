15cc8dda47bebdfa597c4ba778c6b61695f07aee
pinanek23 <hoangsonngo555@gmail.com>
2022-07-14 17:02:34 +0700

    Updated canonical url

--- astro.config.ts
+++ astro.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
 
 export default defineConfig({
+  site: 'http://pinanek23.pages.dev/',
+
   integrations: [solid()]
 })
